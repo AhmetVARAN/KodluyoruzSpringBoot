2fe7b8c24c30f4356409a698b1b38400c913a68f
AhmetVARAN <ahmetvaran2015@gmail.com>
2022-05-24 23:37:55 +0300

    DevTools make active

---
+++ src/main/java/com/ahmetvaran/utils/ConfigProgramming.java
@@ -0,0 +1,9 @@
+package com.ahmetvaran.utils;
+
+public class ConfigProgramming {
+
+    //DevTools ==>LiveReload
+    //file ==> settings ==> compiler ==> Build project automatically (Ctrl+Alt+s)
+    //file ==> Advanced Setting ==> Allow Auto-make to start even (OK clicked) (Ctrl+Alt+s)
+
+}
