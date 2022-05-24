893194d34a58cc4fc7a057a0860734293d4d11f5
AhmetVARAN <ahmetvaran2015@gmail.com>
2022-05-24 23:37:21 +0300

    shortcuts

---
+++ src/main/java/com/ahmetvaran/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.ahmetvaran.utils;
+
+public class IntellijShortCut {
+    //Setting ==> ctrl+alt+s
+    //Alt+Ins ==> constructor, toString, getter, setter
+    //Ctrl+/ ==> single comment
+    //Ctrl+Shift+/ ==> multiple comment
+}
