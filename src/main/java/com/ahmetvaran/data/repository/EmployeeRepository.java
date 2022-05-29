2bd3b9ab5e6055c481bfb7518c6bdbfbb50643f8
AhmetVARAN <ahmetvaran2015@gmail.com>
2022-05-29 22:36:10 +0300

    extends CrudRepository<EmployeeEntity,Long>

--- src/main/java/com/ahmetvaran/data/repository/EmployeeRepository.java
+++ src/main/java/com/ahmetvaran/data/repository/EmployeeRepository.java
@@ -1,10 +1,11 @@
 package com.ahmetvaran.data.repository;
 
+import com.ahmetvaran.data.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 //JpaRepository>CrudRepository
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends CrudRepository<EmployeeEntity,Long> {
 }
